b46cfb107ccb5b7f2aefa927442cdba23cb18fad
Elijah Lowe <elijahlowe@gmail.com>
2014-09-16 17:06:05 -0400

    updated fixes from Titus, fig 2,4 and table 3. Added mocu trinity memory to results

--- scripts/moccVsknowmolg.R
+++ scripts/moccVsknowmolg.R
@@ -20,6 +20,7 @@ p_mapped.melt<-melt(p_mapped_frame,id.vars=1:2)
 p_mapped.melt$Sample<-factor(p_mapped.melt$Sample, as.character(p_mapped.melt$Sample)) #fix bar ordering
 p_mapped_frame$Sample<-factor(p_mapped_frame$Sample, as.character(p_mapped_frame$Sample))
 std <- transform(p_mapped_frame, lower=Values1-stdv, upper=Values1+stdv)
+pdf('/Users/elijahklowe/2014-mrnaseq-cloud/manuscript/figure_4_moccVsknownmolg.pdf',height=8.5,width=11,paper='special')
 ggplot(p_mapped_frame,aes(x=Sample,y=Values1,fill=Sample),beside=True)+
   coord_cartesian(ylim=c(0,105))+
   geom_bar(stat="identity",position=dodge)+facet_grid(. ~ prog)+ #show actual count and bin by treatment
@@ -28,5 +29,6 @@ ggplot(p_mapped_frame,aes(x=Sample,y=Values1,fill=Sample),beside=True)+
   xlab("")+
   theme(axis.text.x = element_blank(),axis.ticks.x = element_blank(),
         text = element_text(size=18))+
-  ggtitle("assemble metrics for M. occulta vs known Molgula nucleotides")+
+  ggtitle("Assembly metrics for M. occulta vs known Molgula nucleotides")+
   geom_errorbar(aes(ymax=upper, ymin=lower), width=0.25, data=std)
+dev.off()
